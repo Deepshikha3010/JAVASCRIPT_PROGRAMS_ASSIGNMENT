65a680b087f3cf5c44648b5f6f39eb83d2b0f029
Deepshikha <deepshika3010@gmail>
2020-09-20 10:44:38 +0530

    first commit

---
+++ JS/Add_two number.js
@@ -0,0 +1,11 @@
+function add(a,b){
+    var x=parseInt(prompt());//String converted into int using parseInt
+    var y=parseInt(prompt());
+    var sum=0;
+    var  sum=x+y;
+   console.log(sum);
+   
+}
+add();
+
+
